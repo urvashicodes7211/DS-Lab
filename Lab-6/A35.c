6ae6f91bda5799164aab11fb2c8bb1b5ce2053cc
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-06-28 13:49:26 +0530

    A35 updated

--- Lab-6/A35.c
+++ Lab-6/A35.c
@@ -3,24 +3,20 @@
 #include<stdio.h>
 int main(){
     int n,i;
+
     printf("Enter n : ");
     scanf("%d",&n);
 
     int a[n];
-    int *ptr;
-    ptr = a;
+    int *ptr = a;
 
     for(i=0;i<n;i++){
         printf("Enter number : ");
-        scanf("%d",ptr);
-        ptr++;
+        scanf("%d", ptr + i);
     } 
 
-    ptr = a;
-
     for(i=0;i<n;i++){
-        printf("%d\n",*ptr);
-        ptr++;
+        printf("%d\n",*(ptr+i));
     }
 
     return 0;
